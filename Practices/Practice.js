11b78aae6a6a1a06a3d4c58b64a4924269a434e2
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-10-21 23:03:26 +0530

    new commit

---
+++ Practices/Practice.js
@@ -0,0 +1,9 @@
+// console.log(a); // ❌ ReferenceError: Cannot access 'a' before initialization
+
+// let a = 10;
+// console.log(a); // ✅ 10
+
+
+console.log(b); // undefined (no error)
+var b = 10;
+console.log(b); // 10
