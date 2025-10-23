1618488f9c6d99a368aaa13c76726023f8243a0f
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-10-23 22:01:18 +0530

    new program

---
+++ Practices/Even_Odd.js
@@ -0,0 +1,13 @@
+// Program to check if a number is even or odd
+
+function checkEvenOdd(number) {
+  if (number % 2 === 0) {
+    console.log(number + " is Even");
+  } else {
+    console.log(number + " is Odd");
+  }
+}
+
+// Example
+checkEvenOdd(10);  // Output: 10 is Even
+checkEvenOdd(7);   // Output: 7 is Odd
