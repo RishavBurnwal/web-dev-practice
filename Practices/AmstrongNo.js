66d37f83e3b10c73c508a7afc07c566b89279cdd
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-11-02 23:33:32 +0530

    new commit

---
+++ Practices/AmstrongNo.js
@@ -0,0 +1,8 @@
+function isArmstrong(num) {
+  const digits = num.toString().split('');
+  const power = digits.length;
+  const sum = digits.reduce((acc, d) => acc + Math.pow(+d, power), 0);
+  return sum === num;
+}
+console.log(isArmstrong(153)); // true
+
