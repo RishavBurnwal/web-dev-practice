cf40680f0897afb83e42817619d32730b1d2c9f7
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-11-15 23:53:40 +0530

    new program

--- Practices/json.js
+++ Practices/json.js
@@ -1,3 +1,3 @@
 let obj = { name: "Rishav", age: 22 };
-let jsonStr = JSON.stringify(obj);
-console.log(JSON.parse(jsonStr));
+let jsonStr = JSON.stringify(obj);  // JSON.stringify(obj) converts the object into a JSON string (text format).
+console.log(JSON.parse(jsonStr));   // JSON.parse(jsonStr) converts that JSON string back into an object.
