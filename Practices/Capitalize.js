489079f548b72cfab7447327ea1fa1d52c9d06b2
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-11-20 23:58:59 +0530

    new program

--- Practices/Capitalize.js
+++
@@ -1,6 +0,0 @@
-function capitalizeWords(str) {
-  return str.split(' ')
-    .map(word => word[0].toUpperCase() + word.slice(1))
-    .join(' ');
-}
-// Example: capitalizeWords("hello world") → "Hello World"
